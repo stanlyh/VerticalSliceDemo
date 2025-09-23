d0b058a5f7efd60f5daf461565844cc150535781
stanlyh <eduardostanly@gmail.com>
2025-09-22 22:07:29 -0500

    Clase para el contexto a la base de datos

---
+++ ContextDB/ConnectionDB.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Data.SqlClient;
+
+namespace VerticalSliceDemo.ContextDB;
+
+public class ConnectionDB(IConfiguration _config)
+{
+    public SqlConnection GetSQL()
+    {
+        return new SqlConnection(_config.GetConnectionString("SqlString"));
+    }
+}
